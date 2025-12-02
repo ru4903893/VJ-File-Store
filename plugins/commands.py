159b873c9f7a74d108802d3147308d582c03b440
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2025-12-02 07:25:49 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,7 +60,6 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            ],[
             InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/movie_search_group1'),
             InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/rdxmovie_hd')
             ],[
